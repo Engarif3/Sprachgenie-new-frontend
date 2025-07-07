481a7fbf8251a3bfc08aafd90d41ec30d109f02f
Md Arifur Rahman <arif.aust.eng@gmail.com>
2025-07-07 20:54:44 +0200

    style={{ caretColor: 'transparent' }} sdded to search input to stop blinking cursor at the lfet side

--- src/View/Words/WordList/WordList.jsx
+++ src/View/Words/WordList/WordList.jsx
@@ -484,8 +484,8 @@ const WordList = () => {
               placeholder="Search for a word"
               value={searchValue}
               onChange={handleSearchInputChange}
-              className="border rounded-l-lg px-2 py-2 w-full md:w-auto flex-1"
-              style={{ caretColor: "transparent" }}
+              className="border rounded-l-lg px-2 py-2 w-full md:w-auto flex-1 "
+              // style={{ caretColor: "transparent" }}
             />
             <button className="bg-blue-500 text-white px-4 py-2 rounded-r-lg hover:bg-blue-600 shrink-0">
               Search
