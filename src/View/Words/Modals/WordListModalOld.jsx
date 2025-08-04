766c1cd4e98729e0454d7f8b916f5bc8769d7445
Md Arifur Rahman <arif.aust.eng@gmail.com>
2025-08-05 00:46:52 +0200

    modal modified

--- src/View/Words/Modals/WordListModalOld.jsx
+++ src/View/Words/Modals/WordListModalOld.jsx
@@ -1,44 +1,29 @@
-import { useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import { getUserInfo, isLoggedIn } from "../../../services/auth.services";
 import { pronounceWord } from "../../../utils/wordPronounciation";
 
 const WordListModal = ({ closeModal, selectedWord }) => {
-  const modalRef = useRef(null);
-
+  if (!selectedWord) return null;
   const userLoggedIn = isLoggedIn();
   const userInfo = getUserInfo();
 
-  // Close on outside click
-  useEffect(() => {
-    const handleClickOutside = (event) => {
-      if (modalRef.current && !modalRef.current.contains(event.target)) {
-        closeModal();
-      }
-    };
-
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => {
-      document.removeEventListener("mousedown", handleClickOutside);
-    };
-  }, [closeModal]);
-
-  if (!selectedWord) return null;
+  // const pronounceWord = (word) => {
+  //   const utterance = new SpeechSynthesisUtterance(word);
+  //   utterance.lang = "de-DE"; // German pronunciation
+  //   speechSynthesis.speak(utterance);
+  // };
 
   return (
-    <div className="fixed z-50 inset-0 bg-black bg-opacity-50 flex justify-center items-center transition-opacity duration-300">
-      <div
-        ref={modalRef}
-        className="bg-white rounded-lg p-2 md:p-8 max-w-4xl w-full shadow-2xl transform transition-all duration-300 scale-105 m-4 border max-h-[90vh] overflow-y-auto"
-      >
-        <p className="text-3xl font-bold text-sky-500 text-center mb-4 border-b border-dashed">
+    <div className="fixed z-50 inset-0 bg-black bg-opacity-50 flex justify-center items-center transition-opacity duration-300  ">
+      <div className="bg-white rounded-lg p-2 md:p-8 lg:p-8 max-w-4xl w-full shadow-2xl transform transition-all duration-300 scale-105 m-4 border">
+        <p className="text-3xl font-bold text-sky-500 text-center mb-4 border-b border-dashed dark:border-gray-600  ">
           <span className="text-orange-500">Topic:</span>{" "}
           {selectedWord.topic?.name || ""}
         </p>
 
-        <div className="flex justify-between items-center">
+        <div className="flex justify-between items-center ">
           <h3 className="text-xl font-semibold text-gray-800 text-center flex items-center gap-4">
-            <span>Word Details</span>
+            <span> Word Details</span>
             <button
               onClick={() => pronounceWord(selectedWord.value)}
               className="text-blue-600 hover:text-blue-800"
@@ -51,6 +36,7 @@ const WordListModal = ({ closeModal, selectedWord }) => {
                 userInfo.role === "admin") && (
                 <Link
                   to={`/edit-word/${selectedWord.id}`}
+                  // onClick={() => onEdit(selectedWord.id)}
                   className="btn btn-sm btn-warning"
                 >
                   Edit
@@ -58,33 +44,49 @@ const WordListModal = ({ closeModal, selectedWord }) => {
               )}
           </h3>
         </div>
-
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-2 md:gap-6 p-2 md:p-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-2 md:gap-6 lg.gap-6  p-2 md:p-4 lg:p-4">
           <p className="text-lg font-medium text-gray-700">
             <span className="font-bold text-sky-500">Word:</span>{" "}
             <span className="capitalize">{selectedWord.value}</span>
           </p>
+
           <p className="text-lg text-gray-600">
             <span className="font-bold text-sky-500 capitalize">Synonyms:</span>{" "}
-            {selectedWord.synonyms?.map((s) => s.value).join(", ") || ""}
+            {selectedWord.synonyms
+              ?.map((synonym) => synonym.value)
+              .join(", ") || ""}
           </p>
+          {/* <p className="text-lg text-gray-600">
+            <span className="font-bold">Article:</span>{" "}
+            {selectedWord.article ? selectedWord.article.name : ""}
+          </p> */}
           <p className="text-lg text-gray-600">
             <span className="font-bold text-sky-500">Meaning:</span>{" "}
             {selectedWord.meaning?.join(", ")}
           </p>
-          <p className="text-lg text-gray-600">
+
+          {/* <p className="text-lg text-gray-600">
+            <span className="font-bold">Part of Speech:</span>{" "}
+            {selectedWord.partOfSpeech?.name}
+          </p> */}
+          <p className="text-lg text-gray-600 ">
             <span className="font-bold text-sky-500 capitalize">Antonyms:</span>{" "}
-            {selectedWord.antonyms?.map((a) => a.value).join(", ") || ""}
+            {selectedWord.antonyms
+              ?.map((antonym) => antonym.value)
+              .join(", ") || ""}
           </p>
-          <p className="text-lg text-gray-600">
+          <p className="text-lg text-gray-600 ">
             <span className="font-bold text-sky-500">Plural:</span>{" "}
-            {selectedWord.pluralForm || ""}
+            {selectedWord.pluralForm ? selectedWord.pluralForm : ""}
           </p>
+
           <p className="text-lg text-gray-600 capitalize">
             <span className="font-bold text-sky-500">Deceptive Word:</span>{" "}
-            {selectedWord.similarWords?.map((sw) => sw.value).join(", ") || ""}
+            {selectedWord.similarWords
+              ?.map((similarword) => similarword.value)
+              .join(", ") || ""}
           </p>
-          <p className="text-lg text-gray-600">
+          <p className="text-lg text-gray-600 ">
             <span className="font-bold text-sky-500">Level:</span>{" "}
             {selectedWord.level?.level || ""}
           </p>
@@ -99,11 +101,10 @@ const WordListModal = ({ closeModal, selectedWord }) => {
                 ))}
               </ul>
             ) : (
-              <span className="text-gray-400">No sentences available.</span>
+              <span className="text-gray-400"> No sentences available.</span>
             )}
           </p>
         </div>
-
         <div className="mt-8 flex justify-center">
           <button
             onClick={closeModal}
