4b40c0a736d8a3a13fd6e8ebec727925bee47a3c
Md Arifur Rahman <arif.aust.eng@gmail.com>
2025-08-31 21:14:59 +0200

    userer creation date added

--- src/AI/ReportsByUsers.jsx
+++ src/AI/ReportsByUsers.jsx
@@ -86,87 +86,88 @@ const ReportsByUsers = () => {
     );
 
   return (
-    <div className="overflow-x-auto mt-4 bg-white">
+    <div className="container mx-auto p-4 min-h-screen">
       <h2 className="text-md md:text-2xl lg:text-2xl font-bold mb-4 text-center  py-2 text-white bg-cyan-700  rounded">
         Reports By Users
       </h2>
-      {/* Table */}
-      <table className="table-auto w-full border border-gray-300 rounded-lg">
-        <thead className="bg-gray-100">
-          <tr>
-            <th className="px-4 py-2 border">Word</th>
-            <th className="px-4 py-2 border">Total Reports</th>
-            <th className="px-4 py-2 border">Reported By</th>
-            <th className="px-4 py-2 border">Message</th>
-          </tr>
-        </thead>
-        <tbody>
-          {reports.map((r, index) => (
-            <tr key={index} className="hover:bg-gray-50 align-top">
-              <td className="px-4 py-2 border">{r.word}</td>
-              <td className="px-4 py-2 border">{r.reportCount}</td>
-              <td className="px-4 py-2 border">
-                {r.reports.map((rep, i) => (
-                  <div key={i} className="mt-2">
-                    {getUserInfo(rep.userId)}
-                    {i < r.reports.length - 1 && (
-                      <hr className="border-gray-300 my-1" />
-                    )}
-                  </div>
-                ))}
-              </td>
-
-              <td className="px-4 py-2 border">
-                {r.reports.map((rep, i) => (
-                  <div key={i}>
-                    <div className="flex justify-between gap-2 items-center mt-2">
-                      {rep.message.length > 25 ? (
-                        <>
-                          <p>{rep.message.slice(0, 10)}...</p>
-                          <button
-                            onClick={() => setSelectedMessage(rep.message)}
-                            className="btn btn-sm btn-accent"
-                          >
-                            View
-                          </button>
-                        </>
-                      ) : (
-                        <p>{rep.message}</p>
+      <div className="overflow-x-auto mt-4 bg-white">
+        {/* Table */}
+        <table className="table-auto w-full border border-gray-300 rounded-lg">
+          <thead className="bg-gray-100">
+            <tr>
+              <th className="px-4 py-2 border">Word</th>
+              <th className="px-4 py-2 border">Total Reports</th>
+              <th className="px-4 py-2 border">Reported By</th>
+              <th className="px-4 py-2 border">Message</th>
+            </tr>
+          </thead>
+          <tbody>
+            {reports.map((r, index) => (
+              <tr key={index} className="hover:bg-gray-50 align-top">
+                <td className="px-4 py-2 border">{r.word}</td>
+                <td className="px-4 py-2 border">{r.reportCount}</td>
+                <td className="px-4 py-2 border">
+                  {r.reports.map((rep, i) => (
+                    <div key={i} className="mt-2">
+                      {getUserInfo(rep.userId)}
+                      {i < r.reports.length - 1 && (
+                        <hr className="border-gray-300 my-1" />
                       )}
                     </div>
-                    {i < r.reports.length - 1 && (
-                      <hr className="border-gray-300 my-1" />
-                    )}
-                  </div>
-                ))}
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+                  ))}
+                </td>
 
-      {/* Modal */}
-      {selectedUser && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-40 z-50">
-          <div className="bg-white rounded-lg p-6 shadow-lg max-w-md w-full">
-            <h2 className="text-lg font-semibold mb-4">User Info</h2>
-            <p>
-              <strong>Name:</strong> {selectedUser.name}
-            </p>
-            <p>
-              <strong>Email:</strong> {selectedUser.email}
-            </p>
-            <p>
-              <strong>User ID:</strong> {selectedUser.id}
-            </p>
-            <div className="mt-4 flex justify-end ">
-              <button
-                onClick={() => setSelectedUser(null)}
-                className="btn btn-sm btn-warning"
-              >
-                Close
-              </button>
-              {/* <button
+                <td className="px-4 py-2 border">
+                  {r.reports.map((rep, i) => (
+                    <div key={i}>
+                      <div className="flex justify-between gap-2 items-center mt-2">
+                        {rep.message.length > 25 ? (
+                          <>
+                            <p>{rep.message.slice(0, 10)}...</p>
+                            <button
+                              onClick={() => setSelectedMessage(rep.message)}
+                              className="btn btn-sm btn-accent"
+                            >
+                              View
+                            </button>
+                          </>
+                        ) : (
+                          <p>{rep.message}</p>
+                        )}
+                      </div>
+                      {i < r.reports.length - 1 && (
+                        <hr className="border-gray-300 my-1" />
+                      )}
+                    </div>
+                  ))}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+
+        {/* Modal */}
+        {selectedUser && (
+          <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-40 z-50">
+            <div className="bg-white rounded-lg p-6 shadow-lg max-w-md w-full">
+              <h2 className="text-lg font-semibold mb-4">User Info</h2>
+              <p>
+                <strong>Name:</strong> {selectedUser.name}
+              </p>
+              <p>
+                <strong>Email:</strong> {selectedUser.email}
+              </p>
+              <p>
+                <strong>User ID:</strong> {selectedUser.id}
+              </p>
+              <div className="mt-4 flex justify-end ">
+                <button
+                  onClick={() => setSelectedUser(null)}
+                  className="btn btn-sm btn-warning"
+                >
+                  Close
+                </button>
+                {/* <button
                 onClick={() => {
                   navigator.clipboard.writeText(selectedUser.id);
                   alert("User ID copied!");
@@ -175,26 +176,27 @@ const ReportsByUsers = () => {
               >
                 Copy ID
               </button> */}
+              </div>
             </div>
           </div>
-        </div>
-      )}
+        )}
 
-      {/* Modal */}
-      {selectedMessage && (
-        <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-          <div className="bg-white p-6 rounded-lg w-96 relative">
-            <button
-              className="absolute top-2 right-2 text-red-600 font-bold"
-              onClick={() => setSelectedMessage(null)}
-            >
-              X
-            </button>
-            <h3 className="text-lg font-bold mb-4">Full Message</h3>
-            <p>{selectedMessage}</p>
+        {/* Modal */}
+        {selectedMessage && (
+          <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+            <div className="bg-white p-6 rounded-lg w-96 relative">
+              <button
+                className="absolute top-2 right-2 text-red-600 font-bold"
+                onClick={() => setSelectedMessage(null)}
+              >
+                X
+              </button>
+              <h3 className="text-lg font-bold mb-4">Full Message</h3>
+              <p>{selectedMessage}</p>
+            </div>
           </div>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   );
 };
