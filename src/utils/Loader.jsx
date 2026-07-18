7a9cb4bb6451a7b3f6ce5b2403e069252f57bb0b
Md.Arifur Rahman <arif.aust.eng@gmail.com>
2026-07-18 04:29:32 +0200

    login button changed

--- src/utils/Loader.jsx
+++ src/utils/Loader.jsx
@@ -1,7 +1,7 @@
 import { ScaleLoader } from "react-spinners";
 const Loader = ({ loading }) => {
   return (
-    <p className="flex justify-center items-center  ">
+    <p className="flex min-h-screen items-center justify-center">
       <span>
         <ScaleLoader
           color="oklch(0.5 0.134 242.749)"
